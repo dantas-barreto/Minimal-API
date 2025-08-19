a0e6236176b488462b318b2e94880040b5155ddd
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2025-08-19 17:53:55 -0300

    Criação de Testes Unitários

---
+++ Test/Domain/Entities/TestAdministrator.cs
@@ -0,0 +1,29 @@
+﻿using MinimalApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Domain.Entities
+{
+    [TestClass]
+    public class TestAdministrator
+    {
+        [TestMethod]
+        public void TestGetSetProperties()
+        {
+            var admin = new Administrator();
+
+            admin.Id = 1;
+            admin.Email = "asdf@asdf.com";
+            admin.Password = "123456";
+            admin.Profile = "Administrator";
+
+            Assert.AreEqual(1, admin.Id);
+            Assert.AreEqual("asdf@asdf.com", admin.Email);
+            Assert.AreEqual("123456", admin.Password);
+            Assert.AreEqual("Administrator", admin.Profile);
+        }
+    }
+}
---
+++ Test/Domain/Entities/TestVehicle.cs
@@ -0,0 +1,27 @@
+﻿using MinimalApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Domain.Entities
+{
+    [TestClass]
+    public class TestVehicle
+    {
+        [TestMethod]
+        public void TestGetSetProperties()
+        {
+            var vehicle = new Vehicle();
+            vehicle.Id = 1;
+            vehicle.Brand = "Chevrolet";
+            vehicle.Model = "Opala Caravan";
+            vehicle.Year = 1985;
+            Assert.AreEqual(1, vehicle.Id);
+            Assert.AreEqual("Toyota", vehicle.Brand);
+            Assert.AreEqual("Camry", vehicle.Model);
+            Assert.AreEqual(2020, vehicle.Year);
+        }
+    }
+}
